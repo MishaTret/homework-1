d05deb79dd72fbde59a3a5909c84ace4831191c8
macbook <mishatret2010@gmail.com>
2023-01-27 22:06:14 +0200

    version 1

---
+++ homework.py
@@ -0,0 +1,38 @@
+class Laptop_Apple:
+    Processor = 'M1 Pro'
+    RAM = '16GB'
+    color = 'Space Gray'
+    name = 'Macbook Pro'
+    Creator = 'Apple'
+
+print(Laptop_Apple.Processor)
+print(Laptop_Apple.RAM)
+print(Laptop_Apple.color)
+print(Laptop_Apple.name)
+print(Laptop_Apple.Creator)
+
+class Roblox:
+    Game = 'true'
+    Graphics = 'medium'
+    Community = 'children'
+    Creator = 'David'
+    Net_worth = '19.62B$'
+
+print(Roblox.Creator)
+print(Roblox.Graphics)
+print(Roblox.Game)
+print(Roblox.Community)
+print(Roblox.Net_worth)
+
+class Python:
+    Founder = 'Guido van Rossum'
+    FoundersNetWorth = '10M$'
+    Difficulty = 'easy'
+    Popular = 'true'
+    Funny = 'true'
+
+print(Python.Founder)
+print(Python.FoundersNetWorth)
+print(Python.Difficulty)
+print(Python.Popular)
+print(Python.Funny)
